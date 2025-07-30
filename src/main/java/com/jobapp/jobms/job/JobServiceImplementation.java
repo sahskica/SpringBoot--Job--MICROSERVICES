6318e7d2914899ddf42c693332554647a636913f
sahskica <sahskica.s@gmail.com>
2025-07-31 01:14:24 +0200

    DTO as a method and using stream

--- src/main/java/com/jobapp/jobms/job/JobServiceImplementation.java
+++ src/main/java/com/jobapp/jobms/job/JobServiceImplementation.java
@@ -1,8 +1,8 @@
 package com.jobapp.jobms.job;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -28,24 +28,22 @@ public class JobServiceImplementation implements JobService{
 	@Override
 	public List<JobWithCompanyDTO> findAll() {
 		List<Job> jobs = jobRepository.findAll();
-		List<JobWithCompanyDTO> jobWithCompanyDTOs = new ArrayList<>();
-		RestTemplate restTemplate = new RestTemplate();
-		
-		for (Job job : 
-				jobs) {
-			JobWithCompanyDTO jobWithCompanyDTO = new JobWithCompanyDTO();
-			jobWithCompanyDTO.setJob(job);
-			
-			Company company = restTemplate.getForObject(
-					"http://localhost:8081/companies/" + job.getCompanyId(), 
-					Company.class);
-			
-			jobWithCompanyDTO.setCompany(company);
-			
-			jobWithCompanyDTOs.add(jobWithCompanyDTO);
-		}
+					
+		return jobs.stream().map(this::convertToDto).collect(Collectors.toList());
+	}
 	
-		return jobWithCompanyDTOs;
+	private JobWithCompanyDTO convertToDto(Job job) {
+		RestTemplate restTemplate = new RestTemplate();
+		JobWithCompanyDTO jobWithCompanyDTO = new JobWithCompanyDTO();
+		jobWithCompanyDTO.setJob(job);
+		
+		Company company = restTemplate.getForObject(
+				"http://localhost:8081/companies/" + job.getCompanyId(), 
+				Company.class);
+		
+		jobWithCompanyDTO.setCompany(company);
+		
+		return jobWithCompanyDTO;
 	}
 
 	@Override
